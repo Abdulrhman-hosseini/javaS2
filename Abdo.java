06b0a2e74543866d4105940f4ccae490b379bb4d
Abdulrhman Hosseini <abdohosseini32@gmail.com>
2025-07-03 16:10:12 +0300

    first commit

---
+++ Abdo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Abdo {
+    public static void main(String[] args) {
+
+        String name = "abdo";
+        int age= 20;
+        System.out.println(name);
+        System.out.println(age);
+
+
+    }
+}
